a3ac281ad0b3249eaca9d45db7ae1586a98a3b7e
Yunus Emre Can Sümeralp <39301904+emresmrlp@users.noreply.github.com>
2024-07-11 09:12:53 +0300

    Add files via upload

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ysumeral <ysumeral@student.42istanbul.c    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 09:47:29 by ysumeral          #+#    #+#             */
-/*   Updated: 2024/07/10 09:48:18 by ysumeral         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -17,16 +5,36 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_print_hex(unsigned char c)
+{
+	int 	first;
+	int	second;
+	char	*base;
+
+	base = "0123456789abcdef";
+	first = c / 16;
+	second = c % 16;
+
+	if(first != 0)
+	{
+		ft_putchar(base[first]);
+		ft_putchar(base[second]);
+	}
+	else
+		ft_putchar('0');
+}
+
 void	ft_putstr_non_printable(char *str)
 {
 	int	i;
+	char	c;
 
-	i = 0;
-	while (str[i] != '\0')
+	while(str[i] != '\0')
 	{
-		if (str[i] <= 32 || str[i] == 127)
+		if(str[i] <= 32 ||  str[i] == 127)
 		{
-			i = 111111111;
+			ft_putchar('\\');
+			ft_print_hex(str[i]);		
 		}
 		i++;
 	}
